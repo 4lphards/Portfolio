5086c8c8c40e64b9777b095686e638504ca8ee82
4lphards <myalghani@gmail.com>
2024-07-25 15:38:05 +0700

    feat: Home Page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Page() {
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   )
 }
@@ -35,19 +36,19 @@ function Navbar() {
         <button className="flex flex-col gap-2 md:hidden z-50" onClick={toggleMenu}>
           {menuOpen ? (
             <>
-              <div className="w-12 h-1.5 bg-white rounded-md transition-transform transform rotate-45 translate-y-1.5"></div>
-              <div className="w-12 h-1.5 bg-white rounded-md transition-transform transform -rotate-45 -translate-y-1.5"></div>
+              <div className="w-10 h-1 bg-white rounded-md transition-transform transform rotate-45 translate-y-1.5"></div>
+              <div className="w-10 h-1 bg-white rounded-md transition-transform transform -rotate-45 -translate-y-1.5"></div>
             </>
           ) : (
             <>
-              <div className="w-12 h-1.5 bg-white rounded-md transition-transform"></div>
-              <div className="w-12 h-1.5 bg-white rounded-md transition-transform"></div>
-              <div className="w-12 h-1.5 bg-white rounded-md transition-transform"></div>
+              <div className="w-10 h-1 bg-white rounded-md transition-transform"></div>
+              <div className="w-10 h-1 bg-white rounded-md transition-transform"></div>
+              <div className="w-10 h-1 bg-white rounded-md transition-transform"></div>
             </>
           )}
         </button>
       </div>
-      <div className={`fixed top-0 right-0 h-16 bg-[#303030] pt-16 pr-6 transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}>
+      <div className={`z-40 fixed top-0 right-0 h-16 bg-transparent pt-16 pr-6 transition-transform transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden`}>
         <ul className="flex flex-col gap-8">
           <li><Link href="#home" onClick={toggleMenu}>Home</Link></li>
           <li><Link href="#about" onClick={toggleMenu}>About</Link></li>
@@ -61,11 +62,32 @@ function Navbar() {
 
 function Home() {
   return (
-    <div id="home" className="h-screen flex items-center justify-center bg-[#303030]">
-      <div className="text-center">
-        <h1 className="text-6xl">Maulana Yusuf</h1>
-        <p className="text-2xl">Software Engineer</p>
+    <div id="home" className="h-screen flex items-center justify-center bg-[#303030] relative overflow-hidden shadow-xl md:justify-between">
+      <div className="absolute -bottom-8 w-full h-5/6 overflow-hidden md:ml-[335px] md:h-[90%] md:mb-12">
+        <Image className="object-contain w-full h-full rotate-6" src="/Image/BgHandDrawn.png" alt="Background Image" layout="fill" />
       </div>
+      <div className="absolute bottom-0 w-11/12 h-[75%] overflow-hidden md:ml-96 md:h-[101%]">
+        <Image className="object-contain filter grayscale contrast-125" src="/Image/Photo Me.png" alt="Overlay Image" layout="fill" />
+      </div>
+      <div className="z-10 pt-8 md:pl-28 md:pt-0 md:pb-16">
+        <div className="text-white text-2xl pt-72 pl-4 relative z-10 md:pt-0 md:text-4xl ">
+          <h1>I'M <span className="text-[#FFA234] text-4xl md:text-6xl">Maulana Yusuf</span></h1>
+          <p>Software Developer</p> 
+        </div>
+        <div className="pl-4 mt-2">
+          <button className={`border-2 md:border-[3px] w-32 h-12 z-10 text-lg rounded-lg md:rounded-lg text-[#FFA234] border-[#FFA234] transition duration-300 ease-in-out transform hover:bg-[#FFA234] hover:text-[#ffffff] hover:scale-105`}>
+            <Link href="#contact">Contact Me</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+
     </div>
   )
 }
